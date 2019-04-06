c1144fadd6f6a355ceabf5291793514e9d056415
Sezgin <kuluser@itu.edu.tr>
2019-04-06 13:39:02 +0300

    Fire Data Added

--- Msg2Client.go
+++ Msg2Client.go
@@ -2,9 +2,13 @@ package main
 
 type CreatorMessage struct {
 	players2Create []PlayerData
+	fires2Create   []FireData
 }
 type UpdateMessage struct {
 	players2Update []PlayerData
+}
+type DestroyMessage struct {
+	players2Destroy []PlayerData
 }
 type Msg2Client struct {
 	creatorMessage CreatorMessage
